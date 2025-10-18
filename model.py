c593e65693e281d3105d615c411d1694b39527b9
LexBwmn <Abowman8556@icloud.com>
2025-10-18 18:21:38 -0400

    model updates (pylint improvements)

--- model.py
+++ model.py
@@ -1,16 +1,15 @@
-# Importing required libs
-from keras.models import load_model
+"""Model loading and prediction functions for image recognition."""
 from keras.utils import img_to_array
 from keras.layers import TFSMLayer
 import numpy as np
 from PIL import Image
 
-# Load the SavedModel as a layer
+# Load the TensorFlow SavedModel using TFSMLayer
 model = TFSMLayer("digit_model_new", call_endpoint="serving_default")
 
 
-# Preparing and pre-processing the image
 def preprocess_img(img_path):
+    """Preprocess the input image for prediction."""
     op_img = Image.open(img_path)
     img_resize = op_img.resize((224, 224))
     img2arr = img_to_array(img_resize) / 255.0
@@ -18,8 +17,8 @@ def preprocess_img(img_path):
     return img_reshape
 
 
-
 def predict_result(img):
+    """Predict the class of the input image using the loaded model."""
     # img should already be preprocessed as a batch (shape: [1, height, width, channels])
     pred = model(img)          # returns a tensor
     pred = np.argmax(pred, axis=-1)  # get predicted class
