1dfad3f1f8ed145e5bf63a57d3f3305288e81595
LexBwmn <Abowman8556@icloud.com>
2025-10-18 17:48:57 -0400

    app.py update (pylint improvements)

--- app.py
+++ app.py
@@ -1,31 +1,25 @@
-# Importing required libs
+"""Flask Image Recognition App"""
 from flask import Flask, render_template, request
 from model import preprocess_img, predict_result
 
-# Instantiating flask app
 app = Flask(__name__)
 
-
-# Home route
 @app.route("/")
 def main():
+    """Flask Image Recognition App"""
     return render_template("index.html")
 
-
-# Prediction route
 @app.route('/prediction', methods=['POST'])
 def predict_image_file():
+    """Handle Image Prediction Request"""
     try:
         if request.method == 'POST':
             img = preprocess_img(request.files['file'].stream)
             pred = predict_result(img)
             return render_template("result.html", predictions=str(pred))
-
     except:
         error = "File cannot be processed."
         return render_template("result.html", err=error)
 
-
-# Driver code
 if __name__ == "__main__":
     app.run(port=9000, debug=True)
