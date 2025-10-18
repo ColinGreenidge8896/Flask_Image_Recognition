d4fd9d563814a6ee474f92e7d1d5c14379703841
LexBwmn <Abowman8556@icloud.com>
2025-10-18 18:16:20 -0400

    convert_model updates (pylint improvements)

--- convert_model.py
+++ convert_model.py
@@ -1,7 +1,7 @@
+"""Convert a Keras .h5 model to TensorFlow SavedModel format."""
+
 from keras.models import load_model
 
-# Load the old .h5 model
 model = load_model("digit_model.h5", compile=False)
 
-# Save it in the new SavedModel format
-model.save("digit_model_saved")  # This creates a folder
+model.save("digit_model_saved")
